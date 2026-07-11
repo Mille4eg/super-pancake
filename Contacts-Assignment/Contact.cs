b3930ccfb86d65dfbbfe66c9524a4415305f3a56
Mille4eg <mille4eg@mail.uc.edu>
2026-07-11 02:50:55 -0400

    Attempt 14

--- Contacts-Assignment/Contact.cs
+++ Contacts-Assignment/Contact.cs
@@ -2,10 +2,10 @@
 {
     public class Contact
     {
-        public string Name { get; set; }
-        public string Image { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
